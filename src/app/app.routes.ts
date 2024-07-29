e8df7744499a7fe77c6a34db4615673968e96de8
getoconas <getoconas@gmail.com>
2024-07-29 19:54:34 -0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FechaComponent } from './components/fecha/fecha.component';
+import { PalabraComponent } from './components/palabra/palabra.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'fecha', component: FechaComponent },
+  { path: 'palabra', component: PalabraComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];

12d07be94a0d309a8f31660cc99319b8d19245a4
getoconas <getoconas@gmail.com>
2024-07-31 15:48:14 -0300

    get sum of input date

--- src/app/components/fecha/fecha.component.ts
+++ src/app/components/fecha/fecha.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-fecha',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule, FormsModule ],
   templateUrl: './fecha.component.html',
   styleUrl: './fecha.component.css'
 })
 export class FechaComponent {
+  txtFecha: string = "";
+  suma: number = 0;
 
+  constructor() {
+  }
+
+  obtenerNumerologia() {
+    this.suma = 0;
+    for (let i = 0; i < this.txtFecha.length; i++) {
+      if (!(this.txtFecha[i] == "-")) {
+        this.suma += parseInt(this.txtFecha[i]);
+      }
+    }
+    console.log(this.suma);
+  }
 }

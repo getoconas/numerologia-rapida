0079d8853904f65efaf176e6a0dce84a2f17c3fa
getoconas <getoconas@gmail.com>
2024-08-04 20:16:07 -0300

    function to get individual number

--- src/app/components/fecha/fecha.component.ts
+++ src/app/components/fecha/fecha.component.ts
@@ -11,18 +11,47 @@ import { RouterModule } from '@angular/router';
 })
 export class FechaComponent {
   txtFecha: string = "";
-  suma: number = 0;
+  resultado: number = 0;
 
   constructor() {
   }
 
   obtenerNumerologia() {
-    this.suma = 0;
-    for (let i = 0; i < this.txtFecha.length; i++) {
-      if (!(this.txtFecha[i] == "-")) {
-        this.suma += parseInt(this.txtFecha[i]);
+    this.resultado = this.obtenerSuma(this.txtFecha);
+    if (this.resultado == 11 || this.resultado == 22 || this.resultado == 33) {
+      console.log('Numero maestro: ' + this.resultado);
+    } else if (this.resultado < 10) {
+      console.log(this.resultado);
+    } else {
+      console.log(" --- " + this.resultado.toString() + " ---");
+      switch (this.resultado.toString()) {
+        case '10':
+        case '19':
+        case '28':
+          console.log('1');
+          break;
+        case '20':
+          console.log('2');
+          break;
+        case '30':
+          console.log('3');
+          break;
+        default:
+          this.resultado = this.obtenerSuma(this.resultado.toString());
+          console.log(this.resultado);
+          break;        
+      } 
+    }
+  }
+
+  // Retorna la suma de los numeros ingresados
+  private obtenerSuma(element: string) {
+    let suma = 0;
+    for (let i = 0; i < element.length; i++) {
+      if (!(element[i] == "-")) {
+        suma += parseInt(element[i]);
       }
     }
-    console.log(this.suma);
+    return suma;
   }
 }

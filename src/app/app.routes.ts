5fbf18b2749730fbeb574750da5b589d0c3321e7
getoconas <getoconas@gmail.com>
2024-08-04 19:01:41 -0300

    configure route compatibilidad component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FechaComponent } from './components/fecha/fecha.component';
 import { PalabraComponent } from './components/palabra/palabra.component';
+import { CompatibilidadComponent } from './components/compatibilidad/compatibilidad.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'fecha', component: FechaComponent },
   { path: 'palabra', component: PalabraComponent },
+  { path: 'compatibilidad', component: CompatibilidadComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];

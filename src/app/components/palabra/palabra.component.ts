b069059eea853fffbd006a6aadd2a121e857472f
getoconas <getoconas@gmail.com>
2024-08-06 20:43:24 -0300

    add search button in word component

--- src/app/components/palabra/palabra.component.ts
+++ src/app/components/palabra/palabra.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-palabra',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule, FormsModule ],
   templateUrl: './palabra.component.html',
   styleUrl: './palabra.component.css'
 })
 export class PalabraComponent {
+  txtPalabra: string = "";
 
+  constructor() {
+  }
+
+  obtenerPalabra() {
+    console.log(this.txtPalabra);
+  }
 }

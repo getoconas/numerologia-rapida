d28af3e2a18ef5287c8f5596a662257b8e31b44c
getoconas <getoconas@gmail.com>
2024-08-12 16:56:57 -0300

    call service compatibilidad in component

--- src/app/components/compatibilidad/compatibilidad.component.ts
+++ src/app/components/compatibilidad/compatibilidad.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Compatibilidad } from '../../models/compatibilidad';
+import { CompatibilidadService } from '../../services/compatibilidad.service';
 
 @Component({
   selector: 'app-compatibilidad',
@@ -10,24 +12,60 @@ import { RouterModule } from '@angular/router';
   styleUrl: './compatibilidad.component.css'
 })
 export class CompatibilidadComponent {
+  listadoCompatibilidades: Array<Compatibilidad>;
   txtFecha1: string = "";
   txtFecha2: string = "";
-  resultado1: number = 0;
-  resultado2: number = 0;
+  numero1: number = 0;
+  numero2: number = 0;
+  mostrarFormulario: boolean;
+  mostrarResultado: boolean;
   
-  constructor() {
-
+  constructor(private compatibilidadService: CompatibilidadService) {
+    this.mostrarFormulario = true;
+    this.mostrarResultado = false;
+    this.listadoCompatibilidades = this.compatibilidadService.obtenerListadoCompatibilidad();
   }
 
   obtenerCompatibilidad() {
-    this.resultado1 = this.obtenerNumero(this.txtFecha1);
-    this.resultado2 = this.obtenerNumero(this.txtFecha2);
-    console.log('--- ' + this.resultado1 + " --- " + this.resultado2 + " ---");
+    if (this.txtFecha1 == '' || this.txtFecha2 == '') {
+      console.log('Error');
+    } else {
+      this.numero1 = this.obtenerNumero(this.txtFecha1);
+      this.numero2 = this.obtenerNumero(this.txtFecha2);
+      this.listadoCompatibilidades.forEach(element => {
+        if (this.obtenerValidacion(element)) {
+          console.log(element);
+        }
+      });
+      //console.log('--- ' + this.numero1 + " --- " + this.numero2 + " ---");
+    }
   }
 
+  private obtenerValidacion(element: any) {
+    return (
+      (element.numero1 == this.numero1 && element.numero2 == this.numero2) ||
+      (element.numero1 == this.numero2 && element.numero2 == this.numero1)
+    );
+  }
+
+  // Retorna una sola cifra 
   private obtenerNumero(element: string) {
     let resultado = this.obtenerSuma(element);
     if (resultado == 11 || resultado == 22 || resultado == 33) {
+      switch (resultado.toString()) {
+        case '11':
+          resultado = 2;
+          break;
+        case '22':
+          resultado = 4;
+          break;
+        case '33':
+          resultado = 6;
+          break;
+        default:
+          resultado = 0
+          break;
+      }
       console.log('Numero maestro: ' + resultado);
     } else if (resultado < 10) {
       console.log(resultado);
@@ -38,14 +76,19 @@ export class CompatibilidadComponent {
         case '19':
         case '28':
         case '37':
+        case '46':
           resultado = 1;
           console.log('1');
           break;
         case '20':
+        case '29':
+        case '38':
+        case '47':
           resultado = 2;
           console.log('2');
           break;
         case '30':
+        case '39':
           resultado = 3;
           console.log('3');
           break;

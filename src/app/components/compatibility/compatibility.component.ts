a1e93bc22550b28217a4383cac86d2294036bda2
getoconas <getoconas@gmail.com>
2025-05-25 19:42:38 -0300

    reestructuring compatibility component

---
+++ src/app/components/compatibility/compatibility.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Compatibility } from '../../models/compatibility';
+import { CompatibilityService } from '../../services/http/compatibility.service';
+import { HelperService } from '../../services/helper.service';
+
+@Component({
+  selector: 'app-compatibilidad',
+  standalone: true,
+  imports: [ RouterModule, FormsModule ],
+  templateUrl: './compatibility.component.html',
+  styleUrl: './compatibility.component.css'
+})
+export class CompatibilityComponent {
+  compatibilityArray: Array<Compatibility>;
+  textDate1: string = "";
+  textDate2: string = "";
+  first_number: number = 0;
+  second_number: number = 0;
+  mostrarFormulario: boolean;
+  mostrarResultado: boolean;
+  
+  constructor(private compatibilityService: CompatibilityService, private helper: HelperService) {
+    this.mostrarFormulario = true;
+    this.mostrarResultado = false;
+    this.compatibilityArray = this.compatibilityService.getCompatibility();
+  }
+
+  public getCompatibility() {
+    if (this.textDate1 == '' || this.textDate2 == '') {
+      console.log('Error');
+    } else {
+      this.first_number = this.helper.getDigit(this.textDate1);
+      this.second_number = this.helper.getDigit(this.textDate2);
+      this.compatibilityArray.forEach(element => {
+        if (this.getValidation(element)) {
+          console.log(element);
+        }
+      });
+    }
+  }
+
+  // Valida si los numeros son iguales
+  private getValidation(element: any) : boolean {
+    return (
+      (element.number_first == this.first_number && element.number_second == this.second_number) ||
+      (element.number_first == this.second_number && element.number_second == this.first_number)
+    );
+  }
+}

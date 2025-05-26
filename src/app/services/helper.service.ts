c181c4a55e0bc976efa505b3ae79d62aacca9010
getoconas <getoconas@gmail.com>
2025-05-26 11:31:10 -0300

    add map letter to number for word component

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -5,6 +5,17 @@ import { ToastrService } from 'ngx-toastr';
   providedIn: 'root'
 })
 export class HelperService {
+  private letterToNumber: { [key: string]: number } = {
+    'a': 1, 'j': 1, 's': 1,
+    'b': 2, 'k': 2, 't': 2,
+    'c': 3, 'l': 3, 'u': 3,
+    'd': 4, 'm': 4, 'v': 4,
+    'e': 5, 'n': 5, 'ñ': 5, 'w': 5,
+    'f': 6, 'o': 6, 'x': 6,
+    'g': 7, 'p': 7, 'y': 7,
+    'h': 8, 'q': 8, 'z': 8,
+    'i': 9, 'r': 9
+  }
 
   constructor(private toastr: ToastrService) { }
 
@@ -38,6 +49,12 @@ export class HelperService {
       number = Math.floor(number / 10);
     }
     return result;
+  }
+
+  // Retorna el numero correspondiente a una letra
+  public getNumberFromLetter(letter: string) : number {
+    const lowerLetter = letter.toLowerCase();
+    return this.letterToNumber[lowerLetter] || 0; // Retorna 0 si la letra no está en el mapa
   }
 
   // Muestra un mensaje de error
